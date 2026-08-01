dd1207e4dd82a2007bdc35fd86812b9ce8184df8
santhi999 <nsanthipriya999@gmail.com>
2026-08-01 08:21:48 -0400

    about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -6,16 +6,19 @@ export default function About()
         <div className="about-container">
           <h1>About Wander USA</h1>
           <p>Wander USA helps you discover America's beautiful national parks.
-            Explore parks by state ,view park information, and save your destinations for your next adventure.
+             Explore parks by state ,view park information, and save your favorite
+             destinations for your next adventure.
           </p>
 
           <p>
-            This application uses the National Park API to provide up-to-date park information,images, descriptions , and activities.
-            You can explore our Wander USA and plan for your fanily's vaction during school breaks.
-            Wander USA is a one-stop solution to explore all parks and decide easily.
+            Explore Wander USA and plan your family's vacation adventures during 
+            school breaks.Wander USA is a one-stop solution to discover beautiful 
+            national parks, explore destinations,and easily choose the perfect places 
+            for your next trip.
 
-            Our goal is to make park exploration simple by providing park information, images, activities, and 
-            favorites so you can save and plan the destinations that interest you most.
+            Our goal is to make park exploration simple by providing park information, 
+            images, activities, and favorites so you can save and plan the destinations 
+            that interest you most.
           </p>
         </div>
 
