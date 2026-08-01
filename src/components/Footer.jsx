da92db5e4efc5146af1516ff87d6b1c4d33a280f
santhi999 <nsanthipriya999@gmail.com>
2026-08-01 10:31:29 -0400

    added website urls

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,8 @@ export default function Footer()
 {
     return (
         <div className="footer">
-            <p>© 2026 Wander USA. All Rights Reserved.</p>
-            <p>Explore America's National Parks</p>
+            <p>© 2026 🦋Wander USA. All Rights Reserved.</p>
+            <p>Explore America's National Parks and attractions.</p>
         </div>
     );
 }
--- src/components/Park.jsx
+++ src/components/Park.jsx
@@ -1,10 +1,9 @@
-export default function Park({park,favorites,addFavorite,removeFavorite}) 
-{
+export default function Park({ park, favorites, addFavorite, removeFavorite }) {
   const isFavorite = favorites?.some((fav) => fav.id === park.id) || false;
   return (
     <div className="display-card">
       <img
-        src={park.images?.length? park.images[0].url: "https://placehold.co/400x300?text=No+Image"}
+        src={park.images?.length ? park.images[0].url : "https://placehold.co/400x300?text=No+Image"}
         alt={park.fullName}
       />
       <div className="card-content">
@@ -12,19 +11,30 @@ export default function Park({park,favorites,addFavorite,removeFavorite})
           {park.fullName}
         </h2>
         <p>
-           {park.states}
+          {park.states}
         </p>
-        <p>{park.description? park.description.substring(0, 15) + "..": "No description available."}
+        <p>{park.description ? park.description.substring(0, 120) + ".." : "No description available."}
         </p>
-        <button
-  onClick={() => {
-    isFavorite
-      ? removeFavorite(park.id)
-      : addFavorite(park);
-  }}
->
-  {isFavorite ? "❤️ Saved" : "🤍 Add Favorite"}
-</button>
+        <div className="card-button">
+          {/* opens the website url in a new tab  */}
+          <a
+            href={park.url}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn More →
+          </a>
+
+          <button
+            onClick={() => {
+              isFavorite
+                ? removeFavorite(park.id)
+                : addFavorite(park);
+            }}
+          >
+            {isFavorite ? "❤️ Saved" : "🤍 Add Favorite"}
+          </button>
+        </div>
       </div>
     </div>
 
