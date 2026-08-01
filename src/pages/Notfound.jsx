f9de50422cc345a43ce849ab11ecea8bc0c1efd8
santhi999 <nsanthipriya999@gmail.com>
2026-08-01 09:41:38 -0400

    added not found

--- src/pages/Notfound.jsx
+++ src/pages/Notfound.jsx
@@ -2,10 +2,12 @@ import { Link } from "react-router";
 
 export default function Notfound() {
   return (
+    <div className="notfound-container">
     <div className="not-found">
       <h1>404</h1>
       <p>Page not found.</p>
       <Link to="/">Return to Home</Link>
+    </div>
     </div>
   );
 }
