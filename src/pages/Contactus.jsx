41e2244dc4ed2d7ada8001a70d3484db257252dc
santhi999 <nsanthipriya999@gmail.com>
2026-08-01 09:23:07 -0400

    contact us

--- src/pages/Contactus.jsx
+++ src/pages/Contactus.jsx
@@ -3,10 +3,12 @@
 export default function Contactus()
 {
     return(
-        <div>
+        <div className="contactus-container">
+        <div className="contactus-card">
           <h2>Contact Us</h2>
-          <p>Have questions about your next adventure? Reach out to us!</p>
+          <p>Have questions about your next adventure? 
+             Reach out to us at explore@wanderusa.com</p>
         </div>
-
+     </div>
     )
 }
