3caece7b87a9a5a08773486951dfb50287750208
santhi999 <nsanthipriya999@gmail.com>
2026-08-01 08:17:32 -0400

    text

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,7 +5,7 @@ export default function About()
     return(
         <div className="about-container">
           <h1>About Wander USA</h1>
-          <p>Wander USA helps you discover Aerica's beautiful natinal parks.
+          <p>Wander USA helps you discover America's beautiful national parks.
             Explore parks by state ,view park information, and save your destinations for your next adventure.
           </p>
 
@@ -13,6 +13,9 @@ export default function About()
             This application uses the National Park API to provide up-to-date park information,images, descriptions , and activities.
             You can explore our Wander USA and plan for your fanily's vaction during school breaks.
             Wander USA is a one-stop solution to explore all parks and decide easily.
+
+            Our goal is to make park exploration simple by providing park information, images, activities, and 
+            favorites so you can save and plan the destinations that interest you most.
           </p>
         </div>
 
