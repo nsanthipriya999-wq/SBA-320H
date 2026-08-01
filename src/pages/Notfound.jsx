aeff00602fba169b7803827239f144844b4ed98b
santhi999 <nsanthipriya999@gmail.com>
2026-08-01 09:44:32 -0400

    added not found styles

--- src/pages/Notfound.jsx
+++ src/pages/Notfound.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router";
 export default function Notfound() {
   return (
     <div className="notfound-container">
-    <div className="not-found">
+    <div className="notfound-card">
       <h1>404</h1>
       <p>Page not found.</p>
       <Link to="/">Return to Home</Link>
